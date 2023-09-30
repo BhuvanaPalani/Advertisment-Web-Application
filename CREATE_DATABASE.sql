5839e6d185ab5869a613c03d2a89d1e77bd63900
bhuvana.p <bhuvi.pavi@gmail.com>
2023-09-30 19:20:53 +0200

    Updated Project

---
+++ CREATE_DATABASE.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE listings_db;
+
+USE listings_db;
+
+CREATE TABLE listings (
+	listing_id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    image_url VARCHAR(2048) NOT NULL,
+    PRIMARY KEY (listing_id)
+);
+
+SELECT * FROM listings;
+
+INSERT INTO listings (title, description, image_url) VALUES
+    ('Some cat', 'This is a cat', 'https://placehold.it/500?text=Cat'),
+    ('Some dog', 'This is a dog', 'https://placehold.it/500?text=Dog'),
+    ('Some parrot', 'This is a parrot', 'https://placehold.it/500?text=Parrot');
